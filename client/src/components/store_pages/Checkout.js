c2ca681ca4c71deac989dcc7bb745195978970bd
Michael Tamietti <michael@Michaels-MacBook-Pro.local>
2018-07-14 12:09:45 -0600

    yarn added recharts, checkout in progress

--- client/src/components/store_pages/Checkout.js
+++ client/src/components/store_pages/Checkout.js
@@ -4,9 +4,9 @@ import { Header, Segment, Button, Form, Message } from 'semantic-ui-react';
 class Checkout extends Component {
   render() {
     return (
-      <Segment style={styles.formAlignment}>
+      <Segment style={styles.segmentAlignment}>
         <Header as='h1' textAlign='center'>Checkout Component</Header>
-        <Form.Input label="First Name" placeholder="First Name" />
+        <Form.Input style={styles.inputAlignment} label="First Name" placeholder="First Name" />
         <Form.Input label="Last Name" placeholder="Last Name" />
         <Form.Input label='Email' placeholder='Email' />
         <Message success header='Form Completed' content="You're all signed up for the newsletter" />
@@ -17,11 +17,12 @@ class Checkout extends Component {
 }
 
 let styles = {
-  formAlignment: {
-    display: 'flex',
+  segmentAlignment: {
     justifyContent: 'center',
-    alignSelf: 'center',
   },
+  inputAlignment: {
+    alignSelf: 'center',
+  }
 
 }
 
